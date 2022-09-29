4c41426c84be9f790e7d7ad0a052109022f7a65b
scottboehmer <scottboehmer@github.com>
2022-09-28 23:03:24 -0400

    experiments and moving more properties to plotter settings

--- TheInnerSphere/SvgPlotter.cs
+++ TheInnerSphere/SvgPlotter.cs
@@ -7,7 +7,7 @@ internal class SvgPlotter
         _text = new List<string>();
         _systems = new List<PlanetInfo>();
 
-        _scale = 10;
+        _scale = settings.Scale;
 
         _width = (int)Math.Ceiling(settings.Width * _scale);
         _height = (int)Math.Ceiling(settings.Height * _scale);
@@ -15,7 +15,13 @@ internal class SvgPlotter
         _centerX = _width / 2.0;
         _centerY = _height / 2.0;
 
-        _palette = settings.Palette;
+        _systemRadius = settings.SystemRadius;
+        _systemPalette = settings.SystemPalette;
+        _subtitleMapping = settings.SystemSubtitleMapping;
+        _linkPalette = settings.LinkPalette;
+
+        _includeJumpLines = settings.IncludeJumpLines;
+        _includeSystemNames = settings.IncludeSystemNames;
     }
 
     public void Add(PlanetInfo system)
@@ -24,18 +30,29 @@ internal class SvgPlotter
         double transformedY = (-1 * _scale * system.Coordinates.Y) + _centerY;
 
         var color = "#ffffff";
-        if (_palette != null)
+        if (_systemPalette != null)
         {
-            color = _palette(system);
+            color = _systemPalette(system);
         }
 
         if (transformedX > 0 && transformedX < _width && transformedY > 0 && transformedY < _height)
         {
-            string svg = $"<circle cx=\"{transformedX}\" cy=\"{transformedY}\" r=\"12\" stroke=\"#000000\" stroke-width=\"1\" fill=\"{color}\" />";
+            string svg = $"<circle cx=\"{transformedX}\" cy=\"{transformedY}\" r=\"{_systemRadius}\" stroke=\"#000000\" stroke-width=\"1\" fill=\"{color}\" />";
             _circles.Add(svg);
 
-            string label = $"<text x=\"{transformedX}\" y=\"{transformedY - 16}\" fill=\"#eeeeee\" text-anchor=\"middle\" font-family=\"sans-serif\" font-size=\"16\" stroke=\"black\" stroke-width=\"0.25\">{system.Name}</text>";
-            _text.Add(label);
+            if (_includeSystemNames)
+            {
+                string label = $"<text x=\"{transformedX}\" y=\"{transformedY - 22}\" fill=\"#eeeeee\" text-anchor=\"middle\" font-family=\"sans-serif\" font-size=\"16\" stroke=\"black\" stroke-width=\"0.25\">{system.Name}</text>";
+                _text.Add(label);
+
+                if (_subtitleMapping != null)
+                {
+                    string subtitle = _subtitleMapping(system);
+                    string subtitleElement = $"<text x=\"{transformedX}\" y=\"{transformedY - 14}\" fill=\"#eeeeee\" text-anchor=\"middle\" font-family=\"sans-serif\" font-size=\"6\" stroke=\"black\" stroke-width=\"0.25\">{subtitle}</text>";
+                    _text.Add(subtitleElement);
+                }
+                
+            }
 
             _systems.Add(system);
         }
@@ -43,7 +60,10 @@ internal class SvgPlotter
 
     public void Write(string file)
     {
-        GenerateLines();
+        if (_includeJumpLines)
+        {
+            GenerateLines();
+        }
         using (var writer = new StreamWriter(file))
         {
             writer.WriteLine("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">");
@@ -67,6 +87,7 @@ internal class SvgPlotter
 
     private void GenerateLines()
     {
+        _lines = new List<string>();
         for (int i = 0; i < _systems.Count; i++)
         {
             var system1 = _systems[i];
@@ -80,7 +101,13 @@ internal class SvgPlotter
                 {
                     double x2 = (_scale * system2.Coordinates.X) + _centerX;
                     double y2 = (-1 * _scale * system2.Coordinates.Y) + _centerY;
-                    string svg = $"<line x1=\"{x1}\" y1=\"{y1}\" x2=\"{x2}\" y2=\"{y2}\" stroke=\"#666666\" stroke-width=\"1\" />";
+
+                    string color = "#666666";
+                    if (_linkPalette != null)
+                    {
+                        color = _linkPalette(system1, system2);
+                    }
+                    string svg = $"<line x1=\"{x1}\" y1=\"{y1}\" x2=\"{x2}\" y2=\"{y2}\" stroke=\"{color}\" stroke-width=\"1\" opacity=\"0.5\" />";
                     _lines.Add(svg);
                 }
             }
@@ -102,5 +129,10 @@ internal class SvgPlotter
     private double _centerX;
     private double _centerY;
     private double _scale;
-    private ColorMapping? _palette;
+    private SystemColorMapping? _systemPalette;
+    private SystemSubtitleMapping? _subtitleMapping;
+    private LinkColorMapping? _linkPalette;
+    private int _systemRadius;
+    private bool _includeJumpLines;
+    private bool _includeSystemNames;
 }
